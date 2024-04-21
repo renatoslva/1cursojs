bce7d974348d95e2a4e64e1091eb801cc4e55981
RENATODEVPROENÇA <101299005+renatoslva@users.noreply.github.com>
2024-04-20 23:54:48 -0300

    novo codico

---
+++ aula14/ex14.js
@@ -0,0 +1,12 @@
+/*repetição simples com teste lógico no inicio*/
+var c = 1 
+while (c <= 6){
+    console.log(`Passo ${c}`)
+    c++
+}
+
+var c = 1 
+do {
+    console.log(`passo ${c}`)
+    c++
+}while (c <= 6)
---
+++ aula14/ex15.js
@@ -0,0 +1,5 @@
+
+
+for (var c = 1; c <= 6; c++){
+    console.log(`contando ${c}`)
+}
