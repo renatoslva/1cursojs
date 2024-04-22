7883ea7c22cd333bdd260d7a48cd02dd1e897085
RENATODEVPROENÇA <101299005+renatoslva@users.noreply.github.com>
2024-04-22 18:59:03 -0300

    novo projeto02

--- aula15/projeto02/script.js
+++ aula15/projeto02/script.js
@@ -1,61 +1,20 @@
-function  verificar(){
-    var data = new Date()
-    var ano = data.getFullYear()
-    var fano = document.getElementById('txtano')
-    var res = document.getElementById('res')
-    if (fano.value.length == 0 || (fano.value) > ano){
-        alert('[ERRO] Verifique os dados e tente novamente...')
+function tabuada(){
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+    if(num.value.length == 0){
+        alert('Por favor, digite um número...')
+    }else{
+        let n = Number(num.value)
+        let c =1
+        tab.innerHTML = ''
+        while(c <= 10){
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            tab.value = `tab${c}`/*isso é para mostar tab 1 tab 2 e assim sucessivamente para o javascript não faz tanto sentido mas para outras linguagem tem muita precisão*/
+            tab.appendChild(item)
+            c++
+        }
+            
+
     }
-    else {
-        var fsex = document.getElementsByName('radsex')
-        var idade = ano - Number(fano.value)
-        var gênero = ''
-        var img = document.createElement('img')
-        img.setAttribute('id', 'foto')
-        if (fsex[0].checked){
-            gênero = 'Homen'
-            if (idade >=0 && idade < 10){
-            //criança
-            img.setAttribute('src', 'fotos/foto-homenbb.jpg')
-        }  else if (idade < 25){
-            //jovem
-            img.setAttribute('src', 'fotos/foto-homenjovem.jpg')
-        }  else if (idade < 50){
-             //adulto
-             img.setAttribute('src', 'fotos/foto-adultohomen.jpg')
-        }else {
-            //idoso 
-            img.setAttribute('src', 'fotos/foto-homenvelho.jpg')}
-          
-        res.innerHTML = `Detectamos ${gênero} com ${idade} Anos.`
-        res.appendChild(img) }
-     
-        else if (fsex[1].checked){
-        gênero = 'Mulher'
-        if (idade >=0 && idade < 10)
-        //criança
-          img.setAttribute('src', 'fotos/foto-mulherbb.jpg')
-     
-     else if (idade < 25){
-        //jovem
-         img.setAttribute('src', 'fotos/foto-mulherjovem.jpg')
-     } 
-     else if (idade < 50){
-        //adulto m 
-         img.setAttribute('src', 'fotos/foto-adultomulher.jpg')
-     }
-     else {
-        //idoso M
-         img.setAttribute('src', 'fotos/foto-mulhervelha.jpg')
-     }
-
-
-     /*res.style.textAlign = 'center' para centralizar sem precisar do css
-     porem esse projeto ja esta centralizado no css*/
-     res.innerHTML = `Detectamos ${gênero} com ${idade} Anos.`
-     res.appendChild(img)
-    
-}
-    }
-
 }
